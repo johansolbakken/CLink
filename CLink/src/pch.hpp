79f62afeb75cb891493dbb3cc9eb6fd2ac62140c
Johan Solbakken <johsol@stud.ntnu.no>
2022-01-23 14:44:47 +0100

    Added precompiled headers

---
+++ CLink/src/pch.hpp
@@ -0,0 +1,18 @@
+#ifndef Pch_hpp
+#define Pch_hpp
+
+#include <iostream>
+#include <memory>
+#include <utility>
+#include <algorithm>
+#include <functional>
+
+#include <string>
+#include <sstream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+
+#include "Link/Core/Log.hpp"
+
+#endif
